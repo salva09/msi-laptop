19a554919fc85a05c00b1d444a47a0758cd1263b
salva01 <salva.hg01@gmail.com>
2022-01-16 01:13:15 -0600

    Update code style

--- msi-laptop.c
+++ msi-laptop.c
@@ -13,12 +13,13 @@ MODULE_VERSION("0.01");
 #define MSIWMI_MSI_EVENT_GUID "B6F3EEF2-3D2F-49DC-9DE3-85BCE18C62F2"
 #define MSIWMI_WIND_EVENT_GUID "5B3CC38A-40D9-7245-8AE6-1145B751BE3F"
 
-static int __init msi_laptop_init(void) {
+static int __init msi_laptop_init(void)
+{
 	printk(KERN_INFO "Loading module\n");
 
 	/*
 	 * I'm going to use this guid because is the only
-	 * one I have to check if this an msi laptop
+	 * one I have to check if this is an msi laptop
 	 */
 	if (wmi_has_guid(MSIWMI_WIND_EVENT_GUID)) {
 		printk(KERN_INFO "Has MSIWMI_WIND_EVENT_GUID!\n");
@@ -27,7 +28,8 @@ static int __init msi_laptop_init(void) {
 	return 0;
 }
 
-static void __exit msi_laptop_exit(void) {
+static void __exit msi_laptop_exit(void)
+{
 	printk(KERN_INFO "Unloading module\n");
 }
 
