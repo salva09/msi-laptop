22aea229df8ef0e13336a01e62c5ae64d1471c45
salva <salva.hg01@gmail.com>
2022-01-15 23:58:19 -0600

    Rename init and exit functions to use the module name

--- msi-laptop.c
+++ msi-laptop.c
@@ -7,14 +7,14 @@ MODULE_AUTHOR("Salvador Hernández");
 MODULE_DESCRIPTION("MSI driver for laptops.");
 MODULE_VERSION("0.01");
 
-static int __init lkm_example_init(void) {
+static int __init msi_laptop_init(void) {
   printk(KERN_INFO "Hello, World!\n");
   return 0;
 }
 
-static void __exit lkm_example_exit(void) {
+static void __exit msi_laptop_exit(void) {
   printk(KERN_INFO "Goodbye, World!\n");
 }
 
-module_init(lkm_example_init);
-module_exit(lkm_example_exit);
+module_init(msi_laptop_init);
+module_exit(msi_laptop_exit);
